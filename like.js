a98e9e5364530020b7e820e832f61b42e7d5203c
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2023-05-02 00:05:30 +0100

    Create like.js
    paste this code into the console to like all tweets on your feed

---
+++ like.js
@@ -0,0 +1,10 @@
+// Wait for 5 seconds
+setTimeout(function() {
+  // Find all elements with class "css-1dbjc4n r-xoduu5"
+  var elements = document.querySelectorAll('.css-1dbjc4n.r-xoduu5');
+
+  // Loop through each element and click on it
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].click();
+  }
+}, 5000);
